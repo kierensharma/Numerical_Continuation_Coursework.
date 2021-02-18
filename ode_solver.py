702feb1f38c5b2f2afcac34664a6a5cde5044510
kierensharma <kieren.sharma@gmail.com>
2021-02-18 22:55:10 +0000

    Updated support for method choice.

--- ode_solver.py
+++ ode_solver.py
@@ -12,27 +12,38 @@ def RK4_step(f,x0,t0,h):
     return x1
 
 # Uses 'euler_step' between two points (x1,t1) and (x2,t2), with step delta_max
-def solve_to(f, x0, t1, t2):
+def solve_to(f, x0, t1, t2, method):
     delta_max = 5
     t_current = t1
     x_current = x0
 
-    while t_current < t2:
-        if t_current + delta_max > t2:
-            delta_max = t2 - t_current
-        
-        x_current = euler_step(f, x_current, t_current, delta_max)
+    if method == 'Euler':
+        while t_current < t2:
+            if t_current + delta_max > t2:
+                delta_max = t2 - t_current
+            
+            x_current = euler_step(f, x_current, t_current, delta_max)
+            t_current = t_current + delta_max
 
-        t_current = t_current + delta_max
+    elif method == 'RK4':
+        while t_current < t2:
+            if t_current + delta_max > t2:
+                delta_max = t2 - t_current
+            
+            x_current = RK4_step()
+            t_current = t_current + delta_max
+    
+    else:
+        print('Please provide a valid method; Euler or RK4')
 
     return x_current
 
 # Uses 'solve_to' to generate a seriese of estimates for x1,x2,x3,...
-def solve_ode(func, x0, t):
+def solve_ode(func, x0, t, method):
     x = np.zeros(len(t))
     x[0] = x0
     for n in range(len(t)-1):
-        x[n+1] = solve_to(func, x[n], t[n], t[n+1])
+        x[n+1] = solve_to(func, x[n], t[n], t[n+1], method)
 
     plt.plot(t,x)
     plt.xlabel('t')
