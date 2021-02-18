3bde89b83efdd065b30765af12851e1c7fbc2573
kierensharma <kieren.sharma@gmail.com>
2021-02-18 13:03:19 +0000

    Commented and working upto solve_ode func.

--- euler_method.py
+++ euler_method.py
@@ -8,24 +8,37 @@ def euler_step(f,x0,t0,h):
     return x1
 
 # Uses 'euler_step' between two points (x1,t1) and (x2,t2), with step delta_max
-def solve_to(f, x0, t0, t2, delta_max):
-    steps = (t2-t0)/delta_max
-    t = np.linspace(t0,t2,steps)
+def solve_to(f, x0, t1, t2):
+    delta_max = 0.01
+    steps = (t2-t1)/delta_max
+    t = np.linspace(t1,t2,steps)
     x = np.zeros(len(t))
     x[0] = x0
 
     for n in range(0,len(t)-1):
         x[n+1] = euler_step(f,x[n],t[n],delta_max)
 
+    return x[-1]
+
+# Uses 'solve_to' to generate a seriese of estimates for x1,x2,x3,...
+def solve_ode(func, x0, t):
+    x = np.zeros(len(t))
+    x[0] = x0
+    for n in range(0,len(t)-1):
+        x[n+1] = solve_to(func, x[n], t[n], t[n+1])
+
     plt.plot(t,x)
     plt.xlabel('t')
     plt.ylabel('x')
     plt.show()
 
-# Uses 'solve_to'
-def solve_ode():
+    return x
 
 
 if __name__ == '__main__':
     func_1 = lambda x,t: x
-    solve_to(f=func_1, x0=1, t0=0, t2=2, delta_max=0.1)
+    x0 = 1
+    t = np.linspace(0,10,100)
+    solve_ode(func_1, x0, t)
+
+    # solve_to(f=func_1, x0=1, t1=0, t2=2)
