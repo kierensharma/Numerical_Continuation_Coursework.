39d2cc85f6540bc3005934284ce76359c6e30cbb
kierensharma <kieren.sharma@gmail.com>
2021-02-18 18:37:22 +0000

    Fixed delta_max within euler_method.

--- euler_method.py
+++ euler_method.py
@@ -2,29 +2,32 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Single Euler step function, with step size h
-def euler_step(f,x0,t0,h):
+def euler_step(f, x0, t0, h):
     x1 =  x0 + h*f(x0,t0)
 
     return x1
 
 # Uses 'euler_step' between two points (x1,t1) and (x2,t2), with step delta_max
 def solve_to(f, x0, t1, t2):
-    delta_max = 0.01
-    steps = (t2-t1)/delta_max
-    t = np.linspace(t1,t2,steps)
-    x = np.zeros(len(t))
-    x[0] = x0
+    delta_max = 5
+    t_current = t1
+    x_current = x0
 
-    for n in range(0,len(t)-1):
-        x[n+1] = euler_step(f,x[n],t[n],delta_max)
+    while t_current < t2:
+        if t_current + delta_max > t2:
+            delta_max = t2 - t_current
+        
+        x_current = euler_step(f, x_current, t_current, delta_max)
 
-    return x[-1]
+        t_current = t_current + delta_max
+
+    return x_current
 
 # Uses 'solve_to' to generate a seriese of estimates for x1,x2,x3,...
 def solve_ode(func, x0, t):
     x = np.zeros(len(t))
     x[0] = x0
-    for n in range(0,len(t)-1):
+    for n in range(len(t)-1):
         x[n+1] = solve_to(func, x[n], t[n], t[n+1])
 
     plt.plot(t,x)
@@ -34,9 +37,11 @@ def solve_ode(func, x0, t):
 
     return x
 
+def func_1(x, t):
+    return x
+
 
 if __name__ == '__main__':
-    func_1 = lambda x,t: x
     x0 = 1
     t = np.linspace(0,10,100)
     solve_ode(func_1, x0, t)
