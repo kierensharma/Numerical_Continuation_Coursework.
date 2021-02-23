5eb5e93590a8dc4c6a59f2f60bb3245cf581d710
kierensharma <kieren.sharma@gmail.com>
2021-02-23 19:08:05 +0000

    Working Euler & RK4 seperate methods & error plots

--- RK4_method.py
+++ RK4_method.py
@@ -2,13 +2,18 @@ import numpy as np
 from matplotlib import pyplot as plt
 from tqdm import tqdm
 
-# Single Euler step function, with step size h
-def euler_step(f, x0, t0, h):
-    x1 =  x0 + h*f(x0,t0)
+# Single 4th-order Runga Kutta step function.
+def RK4_step(f, x0, t0, h):
+    k1 = h*f(x0, t0)
+    k2 = h*f(x0 + 0.5*k1, t0 + 0.5*h)
+    k3 = h*f(x0 + 0.5*k2, t0 + 0.5*h)
+    k4 = h*f(x0 + k3, t0 + h)
+
+    x1 = x0 + (1/6)*(k1 + 2*k2 + 2*k3 + k4)
 
     return x1
 
-# Uses 'euler_step' between two points (x1,t1) and (x2,t2), with step delta_max
+# Uses 'RK4_step' between two points (x1,t1) and (x2,t2), with step delta_max
 def solve_to(f, x0, t1, t2, delta_max):
     t_current = t1
     x_current = x0
@@ -17,7 +22,7 @@ def solve_to(f, x0, t1, t2, delta_max):
         if t_current + delta_max > t2:
             delta_max = t2 - t_current
         
-        x_current = euler_step(f, x_current, t_current, delta_max)
+        x_current = RK4_step(f, x_current, t_current, delta_max)
 
         t_current = t_current + delta_max
 
@@ -42,14 +47,16 @@ if __name__ == '__main__':
     x0 = 1
     ts = np.linspace(0,10,100)
     f = lambda x,t: x
+    x = solve_ode(f, x0, ts, 1)
 
-    # x_true = np.exp(t)
+    # x_true = np.exp(ts)
 
-    # plt.plot(t,x,'b.-',t,x_true,'r-')
-    # plt.legend(['Euler','True'])
+    # plt.plot(ts,x,'b.-',ts,x_true,'r-')
+    # plt.legend(['RK4','True'])
     # plt.grid(True)
     # plt.title("Solution of $x'=x , x(0)=1$")
     # plt.show()
+    
 
     errors = []
     delta_max_range = np.logspace(-6, 0, 20)
@@ -64,6 +71,6 @@ if __name__ == '__main__':
     plt.xlabel('Step-size')
     plt.ylabel('Error')
     plt.grid(True)
-    plt.title("Plot of global error against step-size, for Euler method")
-    plt.savefig('Figures/Euler_error_plot.pdf')
+    plt.title("Plot of global error against step-size, for RK4 method")
+    plt.savefig('Figures/RK4_error_plot.pdf')
     plt.show()
