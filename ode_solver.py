ead815365369da54e2a4fe836d3c591a0624174e
kierensharma <kieren.sharma@gmail.com>
2021-03-09 20:46:37 +0000

    Final ODE system solver

--- ode_solver.py
+++ ode_solver.py
@@ -7,7 +7,7 @@ def main(filename=None):
     X0 = [0, 1]
     t = np.linspace(0,10,200)
 
-    t, x, v = solve_ode(f_shm, X0, t, 1, 'RK4')
+    x, v = solve_ode(f_shm, X0, t, 1, 'RK4')
     sol_true = odeint(f_shm, X0, t)
     true_x_sol= sol_true[:, 0]
     true_v_sol = sol_true[:, 1]
@@ -31,19 +31,18 @@ def euler_step(f, x0, t0, h):
     return x1
 
 # Single 4th-order Runga Kutta step function.
-def RK4_step():
-    x1 = 
-    
+def RK4_step(f, x0, t0, h):
+    k1 = h*f(x0, t0)
+    k2 = h*f(x0 + 0.5*k1, t0 + 0.5*h)
+    k3 = h*f(x0 + 0.5*k2, t0 + 0.5*h)
+    k4 = h*f(x0 + k3, t0 + h)
+
+    x1 = x0 + (1/6)*(k1 + 2*k2 + 2*k3 + k4)
+
     return x1
 
-<<<<<<< HEAD
 # Uses 'euler_step' or 'RK4_step between two points (x1,t1) and (x2,t2), with step delta_max.
 def solve_to(f, x0, t1, t2, delta_max, method):
-=======
-# Uses 'euler_step' between two points (x1,t1) and (x2,t2), with step delta_max.
-def solve_to(f, x0, t1, t2, method):
-    delta_max = 5
->>>>>>> parent of 5eb5e93... Working Euler & RK4 seperate methods & error plots
     t_current = t1
     x_current = x0
 
@@ -69,11 +68,18 @@ def solve_to(f, x0, t1, t2, method):
     return x_current
 
 # Uses 'solve_to' to generate a seriese of estimates for x1,x2,x3,...
-def solve_ode(func, x0, t, delta_max, method):
+def solve_ode(func, X0, t, delta_max, method):
     x = np.zeros(len(t))
-    x[0] = x0
+    v = np.zeros(len(t))
+    Sol = np.column_stack((x, v))
+    Sol[0][0] = X0[0]
+    Sol[0][1] = X0[1]
+
     for n in range(len(t)-1):
-        x[n+1] = solve_to(func, x[n], t[n], t[n+1], delta_max, method)
+        Sol[n+1] = solve_to(func, Sol[n], t[n], t[n+1], delta_max, method)
+
+    x = Sol[:, 0]
+    v = Sol[:, 1]
 
     return x, v
 
@@ -89,21 +95,22 @@ def f_shm(X, t):
     dxdt = v
     dvdt = -x
     dXdt = [dxdt, dvdt]
-    return dXdt
+
+    return np.asarray(dXdt)
 
 
 # Function to plot values of x and t, alongside real solution
 def plot_solution(t, x, v, true_x_sol, true_v_sol):
     fig = plt.figure()
-    plt.set_title('Solution of system of ODEs')
+    plt.title('Solution of system of ODEs')
 
     plt.plot(t, x, color='green', linewidth=2, label=r'$x$')
     plt.plot(t, v, color='blue', linewidth=2, label=r'$v$')
 
-    plt.plot(t, true_x_sol, color='g.-', linewidth=2, label=r'$True x$')
-    plt.plot(t, true_v_sol, color='b.-', linewidth=2, label=r'$True v$')
+    plt.plot(t, true_x_sol, 'g.-', linewidth=2, label=r'$True x$')
+    plt.plot(t, true_v_sol, 'b.-', linewidth=2, label=r'$True v$')
 
-    plt.set_xlabel('t')
+    plt.xlabel('t')
     plt.grid()
     plt.legend()
 
