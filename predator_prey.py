56d73691f4e60fbce5f99545b5d6e84f6a82c8c4
kierensharma <kieren.sharma@gmail.com>
2021-03-18 20:22:44 +0000

    Working predator-prey simulator.

--- predator_prey.py
+++ predator_prey.py
@@ -1,123 +1,80 @@
+# %%
 import numpy as np
-from matplotlib import pyplot as plt
-from tqdm import tqdm
-from scipy.integrate import odeint
+from scipy.integrate import solve_ivp
+from scipy.optimize import root
+from matplotlib import pyplot
+from math import nan
 
-def main(filename=None):
-    X0 = np.array([5, 12])
-    t = np.linspace(0,10,200)
-
-    Sol = solve_ode(predator_prey, X0, t, 1, 'RK4')
-    Sol_true = odeint(predator_prey, X0, t)
-
-    varbs = ['x', 'v']
-    fig = plot_solution(varbs, t, Sol, Sol_true)
-
-    if filename is None:
-        # (show on screen)
-        plt.show()
-    else:
-        # (save to file)
-        fig.savefig(filename)
-
-
-# Single Euler step function, with step size h.
-def euler_step(f, x0, t0, h):
-    x1 =  x0 + h*f(x0,t0)
-
-    return x1
-
-
-# Single 4th-order Runga Kutta step function.
-def RK4_step(f, x0, t0, h):
-    k1 = h*f(x0, t0)
-    k2 = h*f(x0 + 0.5*k1, t0 + 0.5*h)
-    k3 = h*f(x0 + 0.5*k2, t0 + 0.5*h)
-    k4 = h*f(x0 + k3, t0 + h)
-
-    x1 = x0 + (1/6)*(k1 + 2*k2 + 2*k3 + k4)
-
-    return x1
-
-
-# Uses 'euler_step' or 'RK4_step between two points (x1,t1) and (x2,t2), with step delta_max.
-def solve_to(f, x0, t1, t2, delta_max, method):
-    t_current = t1
-    x_current = x0
-
-    if method == 'Euler':
-        while t_current < t2:
-            if t_current + delta_max > t2:
-                delta_max = t2 - t_current
-            
-            x_current = euler_step(f, x_current, t_current, delta_max)
-            t_current = t_current + delta_max
-
-    elif method == 'RK4':
-        while t_current < t2:
-            if t_current + delta_max > t2:
-                delta_max = t2 - t_current
-            
-            x_current = RK4_step(f, x_current, t_current, delta_max)
-            t_current = t_current + delta_max
-    
-    else:
-        print('Please provide a valid method; Euler or RK4')
-
-    return x_current
-
-
-# Uses 'solve_to' to generate a seriese of estimates for x1,x2,x3,...
-def solve_ode(func, X0, t, delta_max, method):
-    Sol = np.zeros((t.size, X0.size))
-    Sol[0] = X0
-
-    for n in range(len(t)-1):
-        Sol[n+1] = solve_to(func, Sol[n], t[n], t[n+1], delta_max, method)
-
-    return Sol
-
-
-# Calculates the global error by suming the differences between the actual and estimated 'x' values
-def error(xs, ts):
-    error =  sum([abs(x-np.exp(t)) for x, t in zip(xs, ts)])
-
-    return error
-
-
-# X = np.arrray([x, y])
-def predator_prey(X, t):
+# %%
+def predator_prey(t, u, b):
     a = 1
-    # b in [0.1, 0.5]
-    b = 0.26
     d = 0.1
 
-    x, y = X
+    x, y = u
     dxdt = x*(1 - x) - (a*x*y)/(d + x)
-    dydt = b*y*(1 - y/x)
+    dydt = (b*y)*(1 - y/x)
     dXdt = np.array([dxdt, dydt])
 
     return dXdt
 
 
-# Function to plot values of x and t, alongside real solution
-def plot_solution(varbs, t, Sol, Sol_true):
-    cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
-    fig = plt.figure()
-    plt.title('Predator-prey System')
+# %%
 
-    for i,v in zip(range(Sol[0].size), varbs):
-        plt.plot(t, Sol[:, i], color=cycle[i], label=v)
-        plt.plot(t, Sol_true[:, i], color=cycle[i], linestyle=':', label='True '+v)
+sol = solve_ivp(lambda t, u: predator_prey(t, u, 0.26), (0, 100), (1.5, 1.75))
+pyplot.plot(sol.t, sol.y[0, :], sol.y[1, :])
 
-    plt.xlabel('t')
-    plt.grid()
-    plt.legend()
+# %%
 
-    return fig
+sol = solve_ivp(lambda t, u: predator_prey(t, u, 0.26), (0, 100), (0.5, 0.75))
+pyplot.plot(sol.y[0, :], sol.y[1, :])
+sol = solve_ivp(lambda t, u: predator_prey(t, u, 0.26), (0, 100), (0.75, 1))
+pyplot.plot(sol.y[0, :], sol.y[1, :])
+sol = solve_ivp(lambda t, u: predator_prey(t, u, 0.26), (0, 100), (1, 1.5))
+pyplot.plot(sol.y[0, :], sol.y[1, :])
+sol = solve_ivp(lambda t, u: predator_prey(t, u, 0.26), (0, 100), (1.5, 1.75))
+pyplot.plot(sol.y[0, :], sol.y[1, :])
+sol = solve_ivp(lambda t, u: predator_prey(t, u, 0.26), (0, 100), (1.75, 2))
+pyplot.plot(sol.y[0, :], sol.y[1, :])
 
+# %%
 
-if __name__ == '__main__':
-    import sys
-    args = sys.argv[1:]
-    main(*args)
+# V nullcline
+Vval = np.linspace(-60, 20, 81)
+Nval = np.zeros(np.size(Vval))
+for (i, V) in enumerate(Vval):
+    result = root(lambda N: predator_prey(nan, (V, N), 0.26)[0], 0)
+    if result.success:
+        Nval[i] = result.x
+    else:
+        Nval[i] = nan
+pyplot.plot(Vval, Nval)
+
+# N nullcline
+Vval = np.linspace(-60, 20, 81)
+Nval = np.zeros(np.size(Vval))
+for (i, V) in enumerate(Vval):
+    result = root(lambda N: morris_lecar(nan, (V, N), 30)[1], 0)
+    if result.success:
+        Nval[i] = result.x
+    else:
+        Nval[i] = nan
+pyplot.plot(Vval, Nval)
+
+# %%
+result = root(lambda u: predator_prey(nan, u, 0.26), (-40, 0))
+if result.success:
+    print("Equilibrium at {}".format(result.x))
+else:
+    print("Failed to converge")
+result = root(lambda u: predator_prey(nan, u, 0.26), (-20, 0))
+if result.success:
+    print("Equilibrium at {}".format(result.x))
+else:
+    print("Failed to converge")
+result = root(lambda u: predator_prey(nan, u, 0.26), (5, 0.3))
+if result.success:
+    print("Equilibrium at {}".format(result.x))
+else:
+    print("Failed to converge")
+
+# %%
