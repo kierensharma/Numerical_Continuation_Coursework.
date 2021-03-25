16a3257e9e4ba35084d638c0fd79c72b48caf138
kierensharma <kieren.sharma@gmail.com>
2021-03-25 21:58:21 +0000

    Working for isolating periodic orbit (limit cycle).

--- ode_shooting.py
+++ ode_shooting.py
@@ -1,9 +1,21 @@
 import numpy as np 
 from scipy.integrate import solve_ivp
 from scipy.optimize import fsolve
+from matplotlib import pyplot as plt
 
 def main():
-    limit_cycle_isolator(predator_prey, (0.5, 2, 40))
+    sol = limit_cycle_isolator(predator_prey, (0.5, 2, 40))
+    plt = phase_portrait_plotter(sol)
+    # plt.plot(sol.t, sol.y[0, :])
+
+    sol = solve_ivp(lambda t, u: predator_prey(t, u), (0, 100), (0.75, 1))
+    plt.plot(sol.y[0, :], sol.y[1, :])
+    sol = solve_ivp(lambda t, u: predator_prey(t, u), (0, 100), (1, 1.5))
+    plt.plot(sol.y[0, :], sol.y[1, :])
+    sol = solve_ivp(lambda t, u: predator_prey(t, u), (0, 100), (1.5, 1.75))
+    plt.plot(sol.y[0, :], sol.y[1, :])
+
+    plt.show()
 
 # Wrapped function for numerical integrator used in 'shooting' method
 def integrate(ode, u0, T):
@@ -23,11 +35,30 @@ def shooting(ode, est):
     
     return np.hstack((u0 - integrate(ode, u0, T), phase_condition(ode, u0, T)))
 
+def orbit(ode, initialu, duration):
+    sol = solve_ivp(ode, (0, duration), initialu)
+    return sol
+
 
 # Function which uses numerical root finder to isolate limit cycles, using 'shooting' function and suitable initial guess ũ0
 def limit_cycle_isolator(ode, est):
-    root = fsolve(lambda est: shooting(ode, est), est)
-    print(root)
+    result = fsolve(lambda est: shooting(ode, est), est)
+    isolated_sol = orbit(ode, result[0:-1], result[-1])
+
+    # plt.plot(isolated_sol.y[0, :], isolated_sol.y[1, :])
+    # plt.show()
+
+    return isolated_sol
+
+def phase_portrait_plotter(sol):
+    plt.plot(sol.y[0, :], sol.y[1, :], label='Isolated periodic orbit')
+    plt.xlabel('$x$')
+    plt.ylabel('$y$')
+    plt.title('Phase portrait')
+    plt.legend()
+    
+    return plt
+
 
 def predator_prey(t, u0):
     a = 1
