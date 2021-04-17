d54bc721f27676bef2ed1e4bdddc922f242fe902
kierensharma <kieren.sharma@gmail.com>
2021-04-17 16:10:26 +0100

    Working error test for Hoph bifurcation

--- shooting_test_script.py
+++ shooting_test_script.py
@@ -1,23 +1,41 @@
 import numpy as np
 from ode_shooting import limit_cycle_isolator as lim
+from ode_shooting import phase_portrait_plotter as phase_plt
 
 def main():
-    result = myfunc(0.5)
-    if abs(result - 2.4794255) < 1e-6:  # value computed with a calculator
-        print("Test passed")
+    shooting_result = lim(hoph_bifurcation, [1, 1, 5], phase_condition)
+    exact_result = explicit_sol(shooting_result.t)
+    error = shooting_result.y - exact_result
+
+    if np.all(abs(error < 1e-2)):
+        print("Passed test")
     else:
-        print("Test failed")
+        print("Failed test")
+
+    plt = phase_plt(shooting_result)
+    plt.show()
 
 def hoph_bifurcation(t, u0):
     sigma = -1 
-    beta = 1
+    beta = 3
 
     u1, u2 = u0
     du1dt = beta*u1 - u2 + sigma*u1*(u1**2 + u2**2)
-    du2dt = u1 + beta*u2 + sigma*u2(u1**2 + u2**2)
+    du2dt = u1 + beta*u2 + sigma*u2*(u1**2 + u2**2)
     dUdt = np.array([du1dt, du2dt])
 
     return dUdt
+
+def explicit_sol(t):
+    phase = 0
+    beta = 3
+
+    u1 = np.sqrt(beta)*np.cos(t + phase)
+    u2 = np.sqrt(beta)*np.sin(t + phase)
+    U = np.array([u1, u2])
+    return U
+
+def phase_condition(ode, u0, T):  return np.array(ode(0, u0)[0])
 
 if __name__ == '__main__':
     main()
