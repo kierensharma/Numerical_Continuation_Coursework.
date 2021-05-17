469c7712c01caa7ad7790aff6f9d4262d63ea50a
kierensharma <kieren.sharma@gmail.com>
2021-05-17 16:24:19 +0100

    Tidying of code into functions.

--- pde_solver.py
+++ pde_solver.py
@@ -1,75 +1,77 @@
-# simple forward Euler solver for the 1D heat equation
-#   u_t = kappa u_xx  0<x<L, 0<t<T
-# with zero-temperature boundary conditions
-#   u=0 at x=0,L, t>0
-# and prescribed initial temperature
-#   u=u_I(x) 0<=x<=L,t=0
-
 import numpy as np
 import pylab as pl
 from math import pi
 
-# Set problem parameters/functions
-kappa = 1.0   # diffusion constant
-L=1.0         # length of spatial domain
-T=0.5         # total time to solve for
-def u_I(x):
+def main():
+    # Set problem parameters/functions
+    kappa = 1.0   # diffusion constant
+    L=1.0         # length of spatial domain
+    T=0.5         # total time to solve for
+
+    # Set numerical parameters
+    mx = 10     # number of gridpoints in space
+    mt = 1000   # number of gridpoints in time
+
+    # Set up the numerical environment variables
+    x = np.linspace(0, L, mx+1)     # mesh points in space
+    t = np.linspace(0, T, mt+1)     # mesh points in time
+    deltax = x[1] - x[0]            # gridspacing in x
+    deltat = t[1] - t[0]            # gridspacing in t
+    lmbda = kappa*deltat/(deltax**2)    # mesh fourier number
+    print("deltax=",deltax)
+    print("deltat=",deltat)
+    print("lambda=",lmbda)
+    if 0 < lmbda < 0.5:
+        print("stability criterion= stable")
+    else:
+        print("stability criterion= unstable")
+
+    u_j = forward_Euler(u_I, x, mx, mt, L, kappa, lmbda)
+
+    # Plot the final result and exact solution
+    pl.plot(x, u_j,'ro',label='num')
+    xx = np.linspace(0,L,250)
+    pl.plot(xx, u_exact(xx,T, L, kappa),'b-',label='exact')
+    pl.xlabel('x')
+    pl.ylabel('u(x,0.5)')
+    pl.legend(loc='upper right')
+    pl.show()
+
+def u_I(x, L):
     # initial temperature distribution
     y = (np.sin(pi*x/L))**2
     return y
 
-def u_exact(x,t):
+def u_exact(x, t, L, kappa):
     # the exact solution
     y = np.exp(-kappa*(pi**2/L**2)*t)*np.sin(pi*x/L)
     return y
 
-# Set numerical parameters
-mx = 10     # number of gridpoints in space
-mt = 1000   # number of gridpoints in time
-
-# Set up the numerical environment variables
-x = np.linspace(0, L, mx+1)     # mesh points in space
-t = np.linspace(0, T, mt+1)     # mesh points in time
-deltax = x[1] - x[0]            # gridspacing in x
-deltat = t[1] - t[0]            # gridspacing in t
-lmbda = kappa*deltat/(deltax**2)    # mesh fourier number
-print("deltax=",deltax)
-print("deltat=",deltat)
-print("lambda=",lmbda)
-if 0 < lmbda < 0.5:
-    print("stability criterion= stable")
-else:
-    print("stability criterion= unstable")
-
-
-# Set up the solution variables
-u_j = np.zeros(x.size)        # u at current time step
-u_jp1 = np.zeros(x.size)     # u at next time step
-
-# Set initial condition
-for i in range(0, mx+1):
-    u_j[i] = u_I(x[i])
-
 # Solve the PDE: in matrix form
-A_FE = np.zeros([mx-1,mx-1])
-np.fill_diagonal(A_FE, (1-2*lmbda))
-np.fill_diagonal(A_FE[1:], lmbda)
-np.fill_diagonal(A_FE[:,1:], lmbda)
+def forward_Euler(u_I, x, mx, mt, L, kappa, lmbda):
+    A_FE = np.zeros([mx-1,mx-1])
+    np.fill_diagonal(A_FE, (1-2*lmbda))
+    np.fill_diagonal(A_FE[1:], lmbda)
+    np.fill_diagonal(A_FE[:,1:], lmbda)
 
-for j in range(0, mt):
-    u_jp1[1:-1] = np.matmul(A_FE, u_j[1:-1].T)
+    # Set up the solution variables
+    u_j = np.zeros(x.size)        # u at current time step
+    u_jp1 = np.zeros(x.size)     # u at next time step
 
-    # Boundary conditions
-    u_jp1[0] = 0; u_jp1[mx] = 0
-        
-    # Save u_j at time t[j+1]
-    u_j[:] = u_jp1[:]
+    # Set initial condition
+    for i in range(0, mx+1):
+        u_j[i] = u_I(x[i], L)
 
-# Plot the final result and exact solution
-pl.plot(x,u_j,'ro',label='num')
-xx = np.linspace(0,L,250)
-pl.plot(xx,u_exact(xx,T),'b-',label='exact')
-pl.xlabel('x')
-pl.ylabel('u(x,0.5)')
-pl.legend(loc='upper right')
-pl.show()
+    for j in range(0, mt):
+        u_jp1[1:-1] = np.matmul(A_FE, u_j[1:-1].T)
+
+        # Boundary conditions
+        u_jp1[0] = 0; u_jp1[mx] = 0
+            
+        # Save u_j at time t[j+1]
+        u_j[:] = u_jp1[:]
+
+    return u_j
+
+if __name__ == "__main__":
+    main()
