7e70465955024cbd376e45d84a9ef64d4d5efbb2
kierensharma <kieren.sharma@gmail.com>
2021-05-17 18:47:10 +0100

    Working Crank-Nicholson

--- pde_solver.py
+++ pde_solver.py
@@ -34,7 +34,8 @@ def main():
         u_0[i] = u_I(x[i], L)
 
     # result = forward_Euler(u_0, mx, mt, lmbda)
-    result = backward_Euler(u_0, mx, mt, lmbda)
+    # result = backward_Euler(u_0, mx, mt, lmbda)
+    result = Crank_Nicholson(u_0, mx, mt, lmbda)
 
     # Plot the final result and exact solution
     pl.plot(x, result,'ro',label='num')
@@ -85,6 +86,30 @@ def backward_Euler(u_j, mx, mt, lmbda):
 
     for j in range(0, mt):
         u_jp1[1:-1] = np.linalg.solve(A_BE, u_j[1:-1].T)
+
+        # Boundary conditions
+        u_jp1[0] = 0; u_jp1[mx] = 0
+            
+        # Save u_j at time t[j+1]
+        u_j[:] = u_jp1[:]
+
+    return u_j
+
+def Crank_Nicholson(u_j, mx, mt, lmbda):
+    u_jp1 = np.zeros(u_j.size)    # u at next time step
+
+    A_CN = np.zeros([mx-1,mx-1])
+    np.fill_diagonal(A_CN, (1+lmbda))
+    np.fill_diagonal(A_CN[1:], -(lmbda/2))
+    np.fill_diagonal(A_CN[:,1:], -(lmbda/2))
+
+    B_CN = np.zeros([mx-1,mx-1])
+    np.fill_diagonal(B_CN, (1-lmbda))
+    np.fill_diagonal(B_CN[1:], lmbda/2)
+    np.fill_diagonal(B_CN[:,1:], lmbda/2)
+
+    for j in range(0, mt):
+        u_jp1[1:-1] = np.linalg.solve(A_CN, np.matmul(B_CN, u_j[1:-1].T))
 
         # Boundary conditions
         u_jp1[0] = 0; u_jp1[mx] = 0
